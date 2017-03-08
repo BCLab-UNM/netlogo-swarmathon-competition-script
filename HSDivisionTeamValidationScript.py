daecd5d5dd6318744a5db861d3c73af65c76a7f4
E. E. Esterly <elizabethesterly@gmail.com>
2017-03-08 06:40:12 -0700

    working in third distribution

---
+++ HSDivisionTeamValidationScript.py
@@ -0,0 +1,10 @@
+##Elizabeth E. Esterly
+##The University of New Mexico
+##NASA Swarmathon HS Division 2017
+##team validation script
+##last revised 03/02/2017
+
+#RULES
+#3 rounds
+#3600 ticks
+#512 resources
--- swarmathon2017autorun.py
+++ swarmathon2017autorun.py
@@ -11,6 +11,18 @@ from py4j.java_gateway import JavaGateway
 from os import listdir
 import sys
 
+def setupRun():
+    bridge.command("setup")
+    bridge.command("ask patches [set pcolor black]")
+
+
+def makeSquareDistribution():
+    bridge.command("ask patches with [pxcor > 30 and pycor > 25] [set pcolor yellow]")
+
+def makeNASALogo():
+    bridge.command("")
+    #ask patches with [distancexy 0 0 < 20 and distancexy 0 0 > 19][set pcolor red]
+    
 #open bridge to Netlogo
 gw = JavaGateway()
 bridge = gw.entry_point
@@ -19,29 +31,36 @@ path = sys.argv[1] #path to code submissions
 files = [c for c in listdir(path) if not (c.startswith('.')) and not c is "parkinglot.jpg"] #toss .DS_store and img file from list of files
 
 for i in range(0, len(files)): #run each file in the path specified in commandline args
-    name = files[i]  
-    print("opening " + name) #shell notification for convenience 
+    name = files[i]
     p = path + name
+    print("opening " + name) #shell notification for convenience 
     bridge.openModel(p)
 
+    ##set world size test
+    bridge.command("resize-world -5 5 -5 5")
+    bridge.exportView("tiny")
+
     #setup the model and setup values for run 1.
-    bridge.command("setup")
-    bridge.command("ask patches [set pcolor black]")
+    setupRun()
+    
     # set 500 random patches to yellow
     bridge.command("ask n-of 500 patches [if pcolor != yellow [set pcolor yellow]]")
     random = bridge.report("count patches with [pcolor = yellow]")
     bridge.exportView(name + "random500.png")
 
     #setup the model and setup values for run 2.
-    bridge.command("setup")
-    bridge.command("ask patches [set pcolor black]")
+    setupRun()
     #make 1 large cluster of 500 patches
-    bridge.command("ask patches with [pxcor > 30 and pycor > 25] [set pcolor yellow]")
+    makeSquareDistribution()
     square = bridge.report("count patches with [pcolor = yellow]")
     bridge.exportView(name + "square500.png")
 
+    #setup the model and setup values for run 3.
+    setupRun()
+    
+
     #tally total score and add to dictionary
-    total = 1000 - random + square
+    total = random + square
 
     ##export performance
     results = sys.argv[2]
