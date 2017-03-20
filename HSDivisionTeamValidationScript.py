861cf9ae17ce575511e70e4b9b0a60632d653899
E. E. Esterly <elizabethesterly@gmail.com>
2017-03-20 07:53:45 -0600

    working beta

--- HSDivisionTeamValidationScript.py
+++ HSDivisionTeamValidationScript.py
@@ -2,9 +2,106 @@
 ##The University of New Mexico
 ##NASA Swarmathon HS Division 2017
 ##team validation script
-##last revised 03/02/2017
+##last revised 03/17/2017
+
+#Python 2.7.12
+
+from py4j.java_gateway import JavaGateway
+from os import listdir
+import sys
+
+global name
+name = sys.argv[1]
+print name
+
+def checkFor(code):
+    if code in open(name).read():
+        illegalCommandError(code)
+    else:
+        ok()
+    
+def illegalCommandError(command):
+    print "WARNING! Illegal command " + command + "found.\n"
+
+def ok():
+    print "OK\n"
+
+def genericError(errorMsg):
+    print "WARNING! " + errorMsg
 
 #RULES
 #3 rounds
 #3600 ticks
 #512 resources
+#Use this script to check if your code violates any rules specified in [Sw5] before submitting your code for the competition. The code will run with whatever values you have set on the slider bars.
+#Put your netlogo file in the same directory with the files used here.
+#Run this script from the command line: python HSDivisionTeamValidationScript.py -yourfilename.nlogo- 
+
+#open bridge to Netlogo and set random seed
+gw = JavaGateway()
+bridge = gw.entry_point
+bridge.openModel(name)
+bridge.command("random-seed 1988")
+
+#get file info and open it
+
+print "Opening" + name + ".\n"
+
+#setup the file
+print "Testing setup function...\n"
+bridge.command("setup")
+bridge.exportView("ttt")
+
+#6 robots
+print "Test for exactly 6 robots: "
+agentCount = bridge.report("count turtles")
+print agentCount
+if agentCount != 6:
+    genericError("You must have 6 robots.\n")
+else:
+    ok()
+
+#robots spawn at the origin
+print "Test for robots spawning at origin.\n"
+atOrigin = bridge.report("count turtles with [xcor != 0 and ycor != 0]")
+print atOrigin
+if atOrigin == "illegal placement":
+    genericError("All 6 robots must begin at (0,0).\n")
+else:
+    ok()
+
+#world size: max-pxcor = max-pycor = 50 and min-pxcor = min-pycor = -50
+print "Test world size.\n"
+maxX = bridge.report("max-pxcor")
+minX = bridge.report("min-pxcor")
+maxY = bridge.report("max-pycor")
+minY = bridge.report("min-pycor")
+if maxX != 50 or maxY != 50 or minX != -50 or minY != -50:
+    print genericError("Your max x and y should be 50 and your min x and y should be -50.\n")
+else:
+    ok()
+
+#Check for illegal commands in the file:
+print "Checking for illegal commands:\n\n"
+illegalCommands = ['move-to', 'setxy', 'hatch', 'die', 'sprout']
+fileProblem = False
+for i in illegalCommands:
+    if checkFor(i):
+        fileProblem = True
+
+if fileProblem:
+    genericError("Please remove the illegal commands in your program.\n")
+
+#robot-control, run for 3600 ticks
+print "Running program for 3600 ticks.\n"
+#for i in range (0, 3600):
+#    bridge.command("robot-control")
+
+#export stats and a view of the screen when finished
+
+
+
+
+
+
+
--- swarmathon2017autorun.py
+++ swarmathon2017autorun.py
@@ -9,12 +9,13 @@
 
 from py4j.java_gateway import JavaGateway
 from os import listdir
-import sys
+import sys   
 
 def setupRun():
+    #set all patches to black and force world size
     bridge.command("setup")
     bridge.command("ask patches [set pcolor black]")
-    bridge.command("resize-world -50 50 -50 50") ##force set world size
+    bridge.command("resize-world -50 50 -50 50") 
 
 def makeRandomDistribution():
     bridge.command("ask n-of 500 patches [if pcolor != yellow [set pcolor yellow]]")
@@ -23,76 +24,89 @@ def makeSquareDistribution():
     bridge.command("ask patches with [pxcor > 30 and pycor > 25] [set pcolor yellow]")
 
 def makeNASALogo():
-    #make circle
-    bridge.comand("ask patches with [distancexy 0 0 < 40 and distancexy 0 0 > 39][set pcolor yellow]")
-   
+    bridge.command("ask patches with [distancexy 0 0 < 40 and distancexy 0 0 > 39][set pcolor yellow]")  #make circle
     #make N 
     bridge.command("ask patches with [pxcor = -30 and pycor >= -8 and pycor <= 8] [set pcolor yellow]")
     bridge.command("ask patches with [pxcor = -20 and pycor >= -8 and pycor <= 8] [set pcolor yellow]")
     bridge.command("ask patches with [pycor >= -1.6  * pxcor + -40 and pycor <= -1.6 * pxcor - 38 and pycor >= -8 and pycor <= 8][set pcolor yellow]")
-    
     #make A
     bridge.command("ask patches with [pycor >= 2 * (pxcor + 11)  and pycor <= 2 * (pxcor + 12) and pycor >= -8 and pycor <= 8][set pcolor yellow]")
     bridge.command("ask patches with [pycor >= -2 * (pxcor + 4)  and pycor <= -2 * (pxcor + 3) and pycor >= -8 and pycor <= 8][set pcolor yellow]")
     bridge.command("ask patches with [pycor = 0 and pxcor >= -11 and pxcor <= -4][set pcolor yellow]")
-    
     #make S
     bridge.command("ask patches with [pycor = -8 and pxcor >= 3 and pxcor <= 11][set pcolor yellow]")
     bridge.command("ask patches with [pycor = 8 and pxcor >= 3 and pxcor <= 11][set pcolor yellow]")
     bridge.command("ask patches with [pycor <= 8 and pycor >= 1 and pxcor = 3 ][set pcolor yellow]")
     bridge.command("ask patches with [pycor = 0 and pxcor >= 3 and pxcor <= 11][set pcolor yellow]")
-    bridge.command("ask patches with [pycor >= -8 and pycor <= 0 and pxcor = 11 ][set pcolor pink]")
-    
+    bridge.command("ask patches with [pycor >= -8 and pycor <= 0 and pxcor = 11 ][set pcolor yellow]")
     #make 2nd A
     bridge.command("ask patches with [pycor >= 2 * (pxcor - 20)  and pycor <= 2 * (pxcor - 19) and pycor >= -8 and pycor <= 8][set pcolor yellow]")
-    bridge.command("ask patches with [pycor >= -2 * (pxcor - 28)  and pycor <= -2 * (pxcor - 29) and pycor >= -8 and pycor <= 8][set pcolor yellow]")
+    bridge.command("ask patches with [pycor >= -2 * (pxcor - 27)  and pycor <= -2 * (pxcor - 28) and pycor >= -8 and pycor <= 8][set pcolor yellow]")
     bridge.command("ask patches with [pycor = 0 and pxcor <= 28 and pxcor >= 20][set pcolor yellow]")
     bridge.command("while[count patches with [pcolor = yellow] < 500][ask one-of patches [if pcolor != yellow and distancexy 0 0 > 40 [set pcolor yellow]]]")
 
-   
+def runOne():
+    setupRun()
+    makeRandomDistribution()
+    return bridge.report("count patches with [pcolor = yellow]")
+
+def runTwo():
+    setupRun()
+    makeSquareDistribution()
+    return bridge.report("count patches with [pcolor = yellow]")
+
+def runThree():
+    setupRun()
+    makeNASALogo()
+    return bridge.report("count patches with [pcolor = yellow]")
     
-#open bridge to Netlogo
+d = {}
+#open bridge to Netlogo and set random seed
 gw = JavaGateway()
 bridge = gw.entry_point
-
+bridge.command("random-seed 1337")
 path = sys.argv[1] #path to code submissions
-files = [c for c in listdir(path) if not (c.startswith('.')) and not c is "parkinglot.jpg"] #toss .DS_store and img file from list of files
+files = [c for c in listdir(path) if not (c.startswith('.')) and not c == 'parkinglot.jpg'] #toss .DS_store and img file from list of files
 
+
+#Competition Loop
 for i in range(0, len(files)): #run each file in the path specified in commandline args
     name = files[i]
     p = path + name
-    print("opening " + name) #shell notification for convenience 
-    bridge.openModel(p)
+    print("opening " + name) #shell notification for convenience
 
-    #setup the model and setup values for run 1.
-    setupRun()
-    # set 500 random patches to yellow
-    makeRandomDistribution()
-    random = bridge.report("count patches with [pcolor = yellow]")
-    bridge.exportView(name + "random500.png")
+    #write all lines except bitmap extension and import to patch colors command
+    print("cleaning" + name)
 
-    #setup the model and setup values for run 2.
-    setupRun()
-    #make 1 large cluster of 500 patches
-    makeSquareDistribution()
-    square = bridge.report("count patches with [pcolor = yellow]")
-    bridge.exportView(name + "square500.png")
+    cleaned = open(path + 'c' + name, 'w')
 
-    #setup the model and setup values for run 3.
-    setupRun()
-    makeNASALogo()
-    bridge.exportView(name + "NASA.png")
+    #read lines one at a time. If you see a b, set flag one and gather chars for the length of that string. Same with e.
+    #When that's done, print it out and do a string comparison. Only write it if they don't match.
+    with open (p, 'r+') as f:
+        while (True):
+            c = f.readline()
+            if c != "  bitmap:copy-to-pcolors bitmap:import \"parkingLot.jpg\" true\n" and c != " extensions[bitmap]\n":
+                cleaned.write(c)
+            if not c:
+                break
+    cleaned.close()
 
-    #tally total score and add to dictionary
-    total = random + square
+    bridge.openModel(path + 'c' + name)
+    random = 500 - runOne() #counts remaining
+    square = 500 - runTwo()
+    logo = 500 - runThree()
+    total = random + square + logo
 
-    ##export performance
-    results = sys.argv[2]
+    #export performance
+    results = sys.argv[2]#file name to store results
     with open (results, 'a') as f:
-        f.write(name + " results\nRandom: " + str(500 - random) + "\nSquare: " + str(500 - square) + "\nTotal: " + str(total) + "\n\n")
+        f.write(name + " results\nRandom: " + str(random) + "\nSquare: " + str(square) + "\nNASA logo" + str(logo) + "\nTotal: " + str(total) + "\n\n")
         f.close()
-        
     bridge.closeModel()
     bridge.refresh()
+
+
+
+    
 
  
